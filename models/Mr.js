fc07b4d83df0ae2ba8a828ca229231217657052b
Anurag Singh <147307671+Anurag-Singh-0@users.noreply.github.com>
2025-11-19 11:30:40 +0530

    Writing logic for Mr to update Mr information

--- models/Mr.js
+++ models/Mr.js
@@ -1,21 +1,31 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  userName: { type: String, unique: true },
-   
-  password: String,
-  displayName:{
-    type: String,
-    default: function() {
-      return `${this.firstName} ${this.lastName}`;
-    }
+const userSchema = new mongoose.Schema(
+  {
+    firstName: String,
+    lastName: String,
+    userName: { type: String, unique: true },
+
+    password: String,
+    displayName: {
+      type: String,
+      default: function () {
+        return `${this.firstName} ${this.lastName}`;
+      },
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    role: { type: String, enum: ["mr", "supervisor"], default: "mr" },
+    organizationId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Organization",
+    },
+    assignedAreas: [{ type: mongoose.Schema.Types.ObjectId, ref: "Area" }],
+    assignedDoctors: [{ type: mongoose.Schema.Types.ObjectId, ref: "Doctor" }],
   },
-  role: { type: String, enum: [ "mr", "supervisor"], default: "mr" },
-  organizationId: { type: mongoose.Schema.Types.ObjectId, ref: "Organization" },
-  assignedAreas: [{ type: mongoose.Schema.Types.ObjectId, ref: "Area" }],
-  assignedDoctors: [{ type: mongoose.Schema.Types.ObjectId, ref: "Doctor" }]
-},{timestamps: true});
+  { timestamps: true }
+);
 
 export default mongoose.model("Mr", userSchema);
